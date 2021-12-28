4e572d7b64706269c3696bda9eed9f3cb2e2fc1f
Arun <86429222+Arun-TG@users.noreply.github.com>
2021-12-28 15:13:55 +0530

    Typo

--- TeamOliver/plugins/Modules/userid.py
+++ TeamOliver/plugins/Modules/userid.py
@@ -3,6 +3,6 @@ from pyrogram import Client, filters
 @Client.on_message(filters.command('id'))
 async def id(bot, message):
     if message.reply_to_message:
-          await message.reply(f"<b>{message.reply_to_message.from_user.mention}'s ID:</b> <code>{message.reply_to_message.from_user.id}</code>)
+          await message.reply(f"<b>{message.reply_to_message.from_user.mention}'s ID:</b> <code>{message.reply_to_message.from_user.id}</code>")
     else:
          await message.reply_text(f"<b>{message.from_user.mention("Your")} ID:</b> <code>{message.from_user.id}</code>}")
