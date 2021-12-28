ff3a82ddcb2bab8d1f75807a67801c07b231933e
Mufaz-TG <92558762+Mufaz-TG@users.noreply.github.com>
2021-12-28 14:54:54 +0530

    Delete userid.py

--- TeamOliver/plugins/userid.py
+++
@@ -1,6 +0,0 @@
-import html
-from pyrogram import Client, filters
-
-@Client.on_message(filters.command('id'))
-async def id(bot, message):
-    await message.reply_text(f"Your id is :-<code>{message.chat.id}</code>}")
