c3121f821d5ccf0e03e380084ac7592310f4afb0
Jσєℓ Bσвαη [Oғғlιɴe] <95572329+JoelBobanOffline@users.noreply.github.com>
2021-12-28 15:22:38 +0530

    Update info.py

--- TeamOliver/plugins/Modules/info.py
+++ TeamOliver/plugins/Modules/info.py
@@ -1,77 +1,23 @@
 from pyrogram import Client, filters
-from TeamOliver.helpers import extract_user, last_online
-import time
-from datetime import datetime
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, Message
 
-@Client.on_message(filters.command(["info"]))
-async def info(client, message):
-    status_message = await message.reply_text(
-        "`Fetching user info...`"
-    )
-    await status_message.edit(
-        "`Processing user info...`"
-    )
-    from_user = None
-    from_user_id, _ = extract_user(message)
-    try:
-        from_user = await client.get_users(from_user_id)
-    except Exception as error:
-        await status_message.edit(str(error))
-        return
-    if from_user is None:
-        return await status_message.edit("no valid user_id / message specified")
-    message_out_str = ""
-    message_out_str += f"<b>➲First Name:</b> {from_user.first_name}\n"
-    last_name = from_user.last_name or "<b>None</b>"
-    message_out_str += f"<b>➲Last Name:</b> {last_name}\n"
-    message_out_str += f"<b>➲Telegram ID:</b> <code>{from_user.id}</code>\n"
-    username = from_user.username or "<b>None</b>"
-    dc_id = from_user.dc_id or "[User Doesnt Have A Valid DP]"
-    message_out_str += f"<b>➲Data Centre:</b> <code>{dc_id}</code>\n"
-    message_out_str += f"<b>➲User Name:</b> @{username}\n"
-    message_out_str += f"<b>➲User 𝖫𝗂𝗇𝗄:</b> <a href='tg://user?id={from_user.id}'><b>Click Here</b></a>\n"
-    if message.chat.type in (("supergroup", "channel")):
-        try:
-            chat_member_p = await message.chat.get_member(from_user.id)
-            joined_date = datetime.fromtimestamp(
-                chat_member_p.joined_date or time.time()
-            ).strftime("%Y.%m.%d %H:%M:%S")
-            message_out_str += (
-                "<b>➲Joined this Chat on:</b> <code>"
-                f"{joined_date}"
-                "</code>\n"
-            )
-        except UserNotParticipant:
-            pass
-    chat_photo = from_user.photo
-    if chat_photo:
-        local_user_photo = await client.download_media(
-            message=chat_photo.big_file_id
-        )
-        buttons = [[
-            InlineKeyboardButton('Close', callback_data='close_data')
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_photo(
-            photo=local_user_photo,
-            quote=True,
-            reply_markup=reply_markup,
-            caption=message_out_str,
-            parse_mode="html",
-            disable_notification=True
-        )
-        os.remove(local_user_photo)
+@Client.on_message(filters.command("info", "whois"))
+def info(_, message):
+    user = message.from_user.id
+    ppic_ct = bot.get_profile_photos_count(user)
+
+    if not ppic_ct == 0:
+        ppic = bot.get_profile_photos(user, limit=1)
+        p_pic = ppic[0]['thumbs'][0]['file_id']
+
+    usr = bot.get_users(user)
+    info = f"""**First Name** : {foo.first_name}
+**Last Name**: {usr.last_name}
+**Id**: {usr.id}
+**Permanent Link**: {usr.mention(message.from_user.first_name)}
+**Is bot**: {usr.is_bot}
+"""
+
+    if ppic_ct != 0:
+        message.reply_photo(p_pic, caption=info)
     else:
-        buttons = [[
-            InlineKeyboardButton('Close', callback_data='close_data')
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_text(
-            text=message_out_str,
-            reply_markup=reply_markup,
-            quote=True,
-            parse_mode="html",
-            disable_notification=True
-        )
-    await status_message.delete()
+        message.reply_text(info)
