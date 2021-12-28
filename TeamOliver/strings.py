d9b4b4d76775bfb88bc2de931f5b8c8ba2c4460e
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-12-28 15:00:39 +0530

    Why TEXT ? Insted of TXT

--- TeamOliver/strings.py
+++ TeamOliver/strings.py
@@ -5,7 +5,7 @@ from TeamOliver import BOT_TOKEN, START_IMAGE
 
 BOT_IMAGE = START_IMAGE
 
-PM_START_TEXT = """
+START_TXT = """
 Heya <b>{}</b>, My name is <b>{}</b>. I am a powerfull. I am a bot
 which can provide movies in your group😘. I have a few extras as well.
 You can check my features by hitting /help 💕.
@@ -18,7 +18,7 @@ Hello {}, contact me in pm if you wanna use me 🤗
 WELCOME_TEXT = """
 hey {}, welcome to {}. So tell us about yourself.
 """
-ABOUT_TEXT = """
+ABOUT_TXT = """
 Owner = <a href="t.me//user?id={}">This person</a><br>
 Source-code = <a href="github.com/TeamOliver/ ">Click here</a><br> #fill this later
 Language = Python3<br>
@@ -27,7 +27,7 @@ library = Pyrogram asyncio<br>
 Creators = <a href="t.me/ ">TeamOliver</a> <br>#fill this later
 """
 
-HELP_TEXT = """
+HELP_TXT = """
 <b>HELP DESK OF {}</b>
 
 - /start - check if I'm alive<br>
