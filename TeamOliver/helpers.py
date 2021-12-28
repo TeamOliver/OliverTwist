82abaad4a4b2f1f296dbf3b89398d370906dea8b
Jσєℓ Bσвαη [Oғғlιɴe] <95572329+JoelBobanOffline@users.noreply.github.com>
2021-12-28 17:16:06 +0530

    no need of these :(

--- TeamOliver/helpers.py
+++ TeamOliver/helpers.py
@@ -1,8 +1,5 @@
 import requests
 from pyrogram.types import Message, InlineKeyboardButton
-from typing import Union
-from datetime import datetime
-from typing import List
 import re
 
 BTN_URL_REGEX = re.compile(
@@ -12,52 +9,6 @@ BTN_URL_REGEX = re.compile(
 SMART_OPEN = '“'
 SMART_CLOSE = '”'
 START_CHAR = ('\'', '"', SMART_OPEN)
-
-def last_online(from_user):
-    time = ""
-    if from_user.is_bot:
-        time += "🤖 Bot"
-    elif from_user.status == 'recently':
-        time += "Recently"
-    elif from_user.status == 'within_week':
-        time += "Within the last week"
-    elif from_user.status == 'within_month':
-        time += "Within the last month"
-    elif from_user.status == 'long_time_ago':
-        time += "A long time ago :("
-    elif from_user.status == 'online':
-        time += "Currently Online"
-    elif from_user.status == 'offline':
-        time += datetime.fromtimestamp(from_user.last_online_date).strftime("%a, %d %b %Y, %H:%M:%S")
-    return time
-
-def extract_user(message: Message) -> Union[int, str]:
-    user_id = None
-    user_first_name = None
-    if message.reply_to_message:
-        user_id = message.reply_to_message.from_user.id
-        user_first_name = message.reply_to_message.from_user.first_name
-
-    elif len(message.command) > 1:
-        if (
-            len(message.entities) > 1 and
-            message.entities[1].type == "text_mention"
-        ):
-           
-            required_entity = message.entities[1]
-            user_id = required_entity.user.id
-            user_first_name = required_entity.user.first_name
-        else:
-            user_id = message.command[1]
-            user_first_name = user_id
-        try:
-            user_id = int(user_id)
-        except ValueError:
-            pass
-    else:
-        user_id = message.from_user.id
-        user_first_name = message.from_user.first_name
-    return (user_id, user_first_name)
 
 def split_quotes(text: str) -> List:
     if any(text.startswith(char) for char in START_CHAR):
