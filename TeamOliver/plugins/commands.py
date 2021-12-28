a0542b1d7571044b43beb2b2bea30cf2aefa3d69
Arun <86429222+Arun-TG@users.noreply.github.com>
2021-12-28 15:29:09 +0530

    Images and some other things

--- TeamOliver/plugins/commands.py
+++ TeamOliver/plugins/commands.py
@@ -1,20 +1,26 @@
 import os
+import random
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardmarkup 
-import random
-from plugins.strings import START_TXT, HELP_TXT, ABOUT_TXT
+from plugins.strings import START_TXT, HELP_TXT, ABOUT_TXT, IMAGES
 
-@Client.on_message(filters.private & filters.command(["start"]))
+@Client.on_message(filters.private & filters.command("start"))
 async def start(client, message):
-    await message.reply_text(
-    text=START_TXT.format(mesage.from_user.mention),
-    reply_markup=InlineKeyboardMarkup(
-    [[
-    InlineKeyboardButton("➕ Add me to your groups", url="https://t.me/{bot_username}/start?group"),
-    InlineKeyboardButton("🤔 Help", callback_data="help")
-    ],[
-    InlineKeyboardButton("🙂 About", callback_data="about")
-    InlineKeyboardButton("❌ Close", callback_data="close")
-    ]])
+    await message.reply_photo(random.choice(IMAGES),
+    caption=START_TXT.format(mention=mesage.from_user.mention,
+                             first=message.from_user.first_name,
+                             last=message.from_user.last_name
+                             username=message.from_user.username),
+    reply_markup=InlineKeyboardMarkup([
+                                       [
+                                         InlineKeyboardButton("➕ Add me to your groups", url="https://t.me/{bot_username}/start?group"),
+                                         InlineKeyboardButton("🤔 Help", callback_data="help")
+                                       ],
+                                       [
+                                         InlineKeyboardButton("🙂 About", callback_data="about")
+                                         InlineKeyboardButton("❌ Close", callback_data="close")
+                                       ]
+                                     ])
+                                )
     
 
