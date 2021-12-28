bc75ebdb545b99161934b081c08864f3c489ce4a
Arun <86429222+Arun-TG@users.noreply.github.com>
2021-12-28 15:13:27 +0530

    reply to a user's message

--- TeamOliver/plugins/Modules/userid.py
+++ TeamOliver/plugins/Modules/userid.py
@@ -2,4 +2,7 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command('id'))
 async def id(bot, message):
-    await message.reply_text(f"Your id is :-<code>{message.from_user.id}</code>}")
+    if message.reply_to_message:
+          await message.reply(f"<b>{message.reply_to_message.from_user.mention}'s ID:</b> <code>{message.reply_to_message.from_user.id}</code>)
+    else:
+         await message.reply_text(f"<b>{message.from_user.mention("Your")} ID:</b> <code>{message.from_user.id}</code>}")
