93f2b58df2d190f03550671273c76668248c65a5
Vaishnav <86590743+Vaishnavofficial@users.noreply.github.com>
2021-12-28 15:39:39 +0530

    Update __main__.py

--- TeamOliver/__main__.py
+++ TeamOliver/__main__.py
@@ -2,7 +2,7 @@
 from pyrogram import Client, filters
 
 from TeamOliver import BOT_TOKEN, API_ID, API, HASH
-from TeamOliver import START_TXT
+from TeamOliver import START_TXT, GROUP_START_TEXT
 
 bot = Client(
        api_id = API_ID,
