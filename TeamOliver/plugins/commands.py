8f75662d2d7bb3d31801247aa0db65af9b19ab3d
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-12-28 14:59:18 +0530

    Update commands.py

--- TeamOliver/plugins/commands.py
+++ TeamOliver/plugins/commands.py
@@ -7,7 +7,7 @@ from plugins.string import START_TXT, HELP_TXT, ABOUT_TXT
 @Client.on_message(filters.private & filters.command(["start"]))
 async def start(client, message):
     await message.reply_text(
-    text=script.START_TXT.format(mesage.from_user.mention),
+    text=START_TXT.format(mesage.from_user.mention),
     reply_markup=InlineKeyboardMarkup(
     [[
     InlineKeyboardButton("➕ Add me to your groups", url="https://t.me/{bot_username}/start?group"),
