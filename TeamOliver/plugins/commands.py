f9e85791f9c9aa2a11887ebbff5093ed74518c7d
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-12-28 14:48:08 +0530

    initial push

---
+++ TeamOliver/plugins/commands.py
@@ -0,0 +1,19 @@
+import os
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardmarkup 
+import random
+
+
+@Client.on_message(filters.private & filters.command(["start"]))
+async def start(client, message):
+    await message.reply_text(
+    text=Script.START_TXT.format(mesage.from_user.mention),
+    reply_markup=InlineKeyboardMarkup(
+    [[
+    InlineKeyboardButton("➕ Add me to your groups", url="https://t.me/{bot_username}/start?group"),
+    InlineKeyboardButton("🤔 Help", callback_data="help")
+    ],[
+    InlineKeyboardButton("🙂 About", callback_data="about")
+    InlineKeyboardButton("❌ Close", callback_data="close")
+    ]])
+    
