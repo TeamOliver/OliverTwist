2af2830290ca8d401ce60f9f02b3cba4285e67eb
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-12-28 15:15:09 +0530

    okay now its , different ui

--- TeamOliver/plugins/commands.py
+++ TeamOliver/plugins/commands.py
@@ -10,10 +10,17 @@ async def start(client, message):
     text=START_TXT.format(mesage.from_user.mention),
     reply_markup=InlineKeyboardMarkup(
     [[
-    InlineKeyboardButton("ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘs", url="https://t.me/{bot_username}/start?group"),
-    InlineKeyboardButton("ʜᴇʟᴘ", callback_data="help")
+    InlineKeyboardButton("➕ Add me to your groups", url="https://t.me/{bot_username}/start?group"),
+    InlineKeyboardButton("🤔 Help", callback_data="help")
     ],[
-    InlineKeyboardButton("ᴀʙᴏᴜᴛ ᴍᴇ", callback_data="about")
-    InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close")
+    InlineKeyboardButton("🙂 About", callback_data="about")
+    InlineKeyboardButton("❌ Close", callback_data="close")
     ]])
     
+@Client.on_message(filters.private & filters.command)
+async def help_about(client, message):
+    await message.reply_text(
+    text=ABOUT_TXT.format(message.from_user.mention), # think different, what should i think different in start message amd buttons - idea by anandhu
+    reply_markup=markups.ABOUT_BUTTONS,
+    disable_web_page_preivew=True
+    )
