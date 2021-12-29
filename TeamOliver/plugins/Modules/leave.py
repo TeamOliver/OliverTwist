09aa223d6c7c26b44cd7b8f13b312d25d52920a1
Arun <86429222+Arun-TG@users.noreply.github.com>
2021-12-29 20:15:31 +0530

    Fix

--- TeamOliver/plugins/Modules/leave.py
+++ TeamOliver/plugins/Modules/leave.py
@@ -1,19 +1,21 @@
 from pyrogram import Client, filters
 from TeamOliver import ADMIN_LIST
 
-USER_ID = message.from_user.id
-@Client.on_message(filters.command('leave') & filters.group)
-async def leave(client, message)
-     if USER_ID in ADMIN_LIST :
-          try:
-             await message.reply_text("Bye, I am leaving this chat")
-            client.leave_chat(.message.chat.id)
-          except exception as e:
-            print(e)
-            message.reply_text("something went wrong")
-     else:
-         print("Wait a minute, who are you..? ")
 
-@Client.on_message(filters.command('leave') & filters.private)
-async def pmleave(client, message)
-     message.reply_message("This can only be used in a group")
+@Client.on_message(filters.command('leave'))
+async def leave(client, message):
+     if message.chat.type = "private":
+          await message.reply("This can only be used in a group")
+     else:
+          if message.from_user.id in ADMIN_LIST :
+               try:
+                  await message.reply_text("Bye, I am leaving this chat")
+                  await client.leave_chat(message.chat.id)
+               except Exception as e:
+                  print(e)
+                  await message.reply_text("something went wrong")
+          else:
+              await message.reply("Wait a minute, who are you..?")
+     
+
+     
