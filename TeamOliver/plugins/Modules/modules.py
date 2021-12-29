af8766db8b380e7a3bfcbdbfbaa30679ff9217ab
Mufaz-TG <92558762+Mufaz-TG@users.noreply.github.com>
2021-12-29 08:10:35 +0530

    Update modules.py

--- TeamOliver/plugins/Modules/modules.py
+++ TeamOliver/plugins/Modules/modules.py
@@ -4,6 +4,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 Button = InlineKeyboardMarkup
 TGButton = InlineKeyboardButton
 Data = callback_data
+Link = url
 BUTTONS = Button(
         [[
         TGButton("Filter", Data="filter_help")
