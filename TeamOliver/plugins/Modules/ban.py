d4d560aef9fa1098ed9db30d673f6afde9549579
Arun <86429222+Arun-TG@users.noreply.github.com>
2021-12-29 06:36:59 +0530

    Fix

--- TeamOliver/plugins/Modules/ban.py
+++ TeamOliver/plugins/Modules/ban.py
@@ -7,10 +7,10 @@ from TeamOliver import ADMIN_LIST
 @Client.on_message(filters.command('ban') & filters.group)
 async def ban(Client, message):
       if message.reply_to_message.from_user.id in ADMIN_LIST :
-           message.reply_text("He is in my adminlist. I cant ban him")
+           message.reply_text("He is in my adminlist. I can't ban him")
       else:
            try:
-               Client.kick_chat_member(message.chat.id, message.reply_to_message.from_user.mention)
+               Client.kick_chat_member(message.chat.id, message.reply_to_message.from_user.id)
                message.reply_text(f"{message.reply_to_message.from_user.first_name} has been banned.")
             except Exception as e:
                print(e)
