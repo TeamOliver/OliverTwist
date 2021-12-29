e0c62d73cfa050bd6bce5d8e4512b91890272135
Jσєℓ Bσвαη [Oғғlιɴe] <95572329+JoelBobanOffline@users.noreply.github.com>
2021-12-29 20:59:30 +0530

    i think done

--- TeamOliver/plugins/Modules/pin.py
+++ TeamOliver/plugins/Modules/pin.py
@@ -1,10 +1,19 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
+async def admins(client, message):
+    x = await client.get_chat_member(message.chat.id, message.from_user.id)
+    if x.status == ("administrator" or "creator"):
+        return True
+    else:
+        return False
+
 
 @Client.on_message(filters.command("pin") & filters.group)
 async def pin(client, message):
-    try:
+    if not admin:
+        return
+    else:
         message_id = message.reply_to_message.message_id
         await client.pin_chat_message(message.chat.id, message_id)
         await message.edit("<code>Pinned successfully!</code>")
@@ -13,11 +22,13 @@ async def pin(client, message):
 
 @Client.on_message(filters.command("unpin") & filters.group)
 async def unpin(client, message):
-    try:
+    if not admin:
+        return
+    else:
         message_id = message.reply_to_message.message_id
         await client.unpin_chat_message(message.chat.id, message_id)
         await message.edit("<code>Unpinned successfully!</code>")
     except:
         await message.edit("Reply to the message you want to unpin")
 
-# Need to check if the user is admin :(
+# Need to fix something
