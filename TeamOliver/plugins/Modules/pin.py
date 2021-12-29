794f6cd7f0298b5ca6a7312272f5295e73dcee4e
Jσєℓ Bσвαη [Oғғlιɴe] <95572329+JoelBobanOffline@users.noreply.github.com>
2021-12-29 20:12:21 +0530

    Update pin.py

--- TeamOliver/plugins/Modules/pin.py
+++ TeamOliver/plugins/Modules/pin.py
@@ -11,4 +11,13 @@ async def pin(client, message):
     except:
         await message.edit("Reply to the message you want to pin")
 
-# Need to add unpin function
+@Client.on_message(filters.command("unpin") & filters.group)
+async def unpin(client, message):
+    try:
+        message_id = message.reply_to_message.message_id
+        await client.pin_chat_message(message.chat.id, message_id)
+        await message.edit("<code>Unpinned successfully!</code>")
+    except:
+        await message.edit("Reply to the message you want to unpin")
+
+# Need to check if the user is admin :(
