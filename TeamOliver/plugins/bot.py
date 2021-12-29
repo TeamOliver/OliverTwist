bc275b0fecd6c6fae4a0a87ff03d8808326ed34c
Mufaz-TG <92558762+Mufaz-TG@users.noreply.github.com>
2021-12-29 07:43:38 +0530

    Update bot.py

--- TeamOliver/plugins/bot.py
+++ TeamOliver/plugins/bot.py
@@ -12,14 +12,16 @@ async def start(client, message):
                              first=message.from_user.first_name,
                              last=message.from_user.last_name,
                              username=message.from_user.username),
-    reply_markup=InlineKeyboardMarkup([
-                                       [InlineKeyboardButton("➕ Add me to your groups", url="https://t.me/{bot_username}/start?group".format(bot_username)],
-                                       [InlineKeyboardButton("❓️ Help", callback_data="help")],
-                                       [InlineKeyboardButton("🗨️ About", callback_data="about")
-                                        InlineKeyboardButton("❌ Close", callback_data="close")],
-                                       [InlinekeyboardButton("📄Source code", url="https://github.com/TeamOliver/OliverTwist")]
-                                     ])
-                              )
+    reply_markup=InlineKeyboardMarkup([[
+                                      InlineKeyboardButton("➕ Add me to your groups", url=f"https://t.me/{bot_username}/start?group".format(bot_username)
+                                      ],[
+                                      InlineKeyboardButton("❓️ Help", callback_data="help"),
+                                      InlineKeyboardButton("🗨️ About", callback_data="about")
+                                      ],[
+                                      InlineKeyboardButton("❌ Close", callback_data="close"),
+                                      InlinekeyboardButton("📄Source code", url="https://github.com/TeamOliver/OliverTwist")
+                                      ]]))
+
 @Client.on_message(filters.command('start') & filters.group)
 async def grpstart(client, message):
     await message.reply_photo(
