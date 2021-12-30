d9e4f9db6d789bdab430072cec9ae07617a3105c
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-12-30 14:05:29 +0530

    is the import is correct ? #6

--- TeamOliver/__main__.py
+++ TeamOliver/__main__.py
@@ -2,7 +2,7 @@
 from pyrogram import Client, filters
 
 from config import BOT_TOKEN, APP_ID, API, HASH
-from config import START_TXT, GROUP_START_TEXT
+from TeamOliver import START_TXT, GROUP_START_TEXT
 
 bot = Client(
        api_id = APP_ID,
