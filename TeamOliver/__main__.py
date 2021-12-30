ce04d45d745fe580685d55f4a13cbd2a71c1795a
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-12-30 14:04:28 +0530

    Update __main__.py

--- TeamOliver/__main__.py
+++ TeamOliver/__main__.py
@@ -1,8 +1,8 @@
 #© TeamOliver
 from pyrogram import Client, filters
 
-from TeamOliver import BOT_TOKEN, APP_ID, API, HASH
-from TeamOliver import START_TXT, GROUP_START_TEXT
+from config import BOT_TOKEN, APP_ID, API, HASH
+from config import START_TXT, GROUP_START_TEXT
 
 bot = Client(
        api_id = APP_ID,
