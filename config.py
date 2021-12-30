6b094c23ecce20901b4afd6b4a2aa9603728fcb3
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-12-30 14:06:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
 from os import environ
 
+# vars that need to run the bot
+API_ID = int(environ['API_ID'])
+API_HASH = environ['API_HASH']
+BOT_TOKEN = environ['BOT_TOKEN']
 
 # command handle
 COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
