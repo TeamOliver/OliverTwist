fe7eadbce5130d54ed59fef14389e269bd986d56
Vaishnav <86590743+Vaishnavofficial@users.noreply.github.com>
2022-01-25 14:52:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,3 @@ API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
 DATABASE_URL = environ['DATABASE_URL']
-
-# command handle
-COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
