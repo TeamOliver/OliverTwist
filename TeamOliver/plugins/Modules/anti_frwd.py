262907fbd52c5f6e41b4a6276d10f9b3c2fe65fd
Vaishnav <86590743+Vaishnavofficial@users.noreply.github.com>
2022-01-25 15:05:48 +0530

    Moonji :-(

---
+++ TeamOliver/plugins/Modules/anti_frwd.py
@@ -0,0 +1,15 @@
+from pyrogram import filters as Oliver
+
+async def antifrwd(Client, message):
+    args = message.from_user.split(None, 1) #chumma showk
+    test = args[1].lower()
+    ON_LIST = ["on", "yes"]
+    OFF_LIST = ["off", "no"]
+    if test in ON_LIST:
+        FRWD = True
+    if test in OFF_LIST:
+        FRWD = False
+@bot.on_message(Oliver.forwarded)
+async def delete_chey(Client, message):
+      while FRWD is True:
+          Client.delete_message()
